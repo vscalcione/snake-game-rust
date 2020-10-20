dba7d3fd03a98c7a8cb92a86e14e072d533f5cc9
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-20 17:24:17 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate piston_window;
+extern crate rand;
+
+mod snake;
+mod game;
+mod drawing;
+
+use piston_window::*;
+use piston_window::types::Color;
+
+use game::Game;
+use drawing::to_gui_coord_u32;
+
+const BACK_COLOR: Color = [0.204, 0.286, 0.369, 1.0];
+
+fn main() {
+    let (width, height) = (20, 20);
+
+    // Create a window
+    let mut window: PistonWindow = WindowSettings::new("Rust Snake",
+        [to_gui_coord_u32(width), to_gui_coord_u32(height)]).exit_on_esc(true).build().unwrap();
+
+    // Create a snake
+    let mut game = Game::new(width, height);
+
+    // Event loop
+    while let Some(event) = window.next() {
+
+        // Catch the events of the keyboard
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+
+        // Draw all of them
+        window.draw_2d(&event, |c, g| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        // Update the state of the game
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
+}
